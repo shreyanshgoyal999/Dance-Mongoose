a6aeae083f11ac344941039cc41d7069bba0b5eb
shreyansh goyal <shreyanshgoyal999@gmail.com>
2021-06-14 17:46:34 +0530

    abs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
                                     // Database name                              
 
 
